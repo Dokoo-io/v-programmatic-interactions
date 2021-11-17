df30b92bc98b6b04640026f0fac65c3965396059
Mathieu Marteau <marteau.mathieu@gmail.com>
2021-11-17 11:55:57 +0100

    Add fullscreen option on modal

--- lib/src/components/components/ProgrammaticModal.vue
+++ lib/src/components/components/ProgrammaticModal.vue
@@ -1,5 +1,5 @@
 <template>
-  <v-dialog :value="true" :max-width="options.maxWidth || 800" persistent>
+  <v-dialog :value="true" :max-width="options.fullscreen ? undefined : (options.maxWidth || 800)" persistent :fullscreen="options.fullscreen">
     <component v-bind="options.options || {}" :is="options.component()" @close="$emit('close')" />
   </v-dialog>
 </template>
